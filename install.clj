f1d42f7bc172d9ffdf51419d17b5d7792dabe70e
Somē Cho <itssomicho@gmail.com>
2023-05-06 19:20:42 +0200

    remove uninstall script, install script creates an uninstall script

--- install.clj
+++ install.clj
@@ -5,18 +5,32 @@
 (defn drop-extension [filename]
   (first (split filename #"\.")))
 
+(defn create-uninstall-script [scripts path filename]
+  (spit (str path filename) "#!/usr/bin/env bb\n")
+  (spit (str path filename)
+        `(do
+           (def a# ~scripts)
+           (doseq [b# a#]
+             (clojure.java.io/delete-file (str ~path b#)))
+           (clojure.java.io/delete-file (str ~path ~filename))) :append true))
+
 (let [files (-> (sh "ls")
                 (:out)
                 (split #"\n"))
       cljfiles (filter #(includes? % ".clj") files)
       scripts (filter #(not= % "install.clj") cljfiles)
+      extensionless  (vec (map drop-extension scripts))
       home (System/getProperty "user.home")
-      path "/.local/bin/"]
+      path "/.local/bin/"
+      uninstall "uninstall-some-scripts"]
   (doseq [script scripts]
     (sh "cp" script (str home path (drop-extension script))))
+  (create-uninstall-script extensionless (str home path) uninstall)
+  (sh "chmod" "+x" (str home path uninstall))
   (println "The following scripts have been copied to '~/.local/bin':\n")
   (doseq [script scripts]
     (println (drop-extension script)))
   (println)
   (println "To use them please ensure that '~/.local/bin` is in your PATH.")
-  (println "i.e. export PATH=~/.local/bin:$PATH"))
+  (println "i.e. export PATH=~/.local/bin:$PATH\n")
+  (println "To uninstall call the" uninstall "script"))
