c321382e86f63cb1b43c2b0bba31fcc422212e1f
Somē Cho <itssomicho@gmail.com>
2023-05-10 09:51:46 +0200

    fix path logic for finding data

--- on-modify-log.clj
+++ on-modify-log.clj
@@ -1,8 +1,10 @@
 #!/usr/bin/env bb
-(require '[cheshire.core :as json])
+(require '[cheshire.core :as json]
+         '[babashka.fs :as fs])
 
 (let [oldtask (json/parse-string (read-line) true)
       newtask (json/parse-string (read-line) true)
-      uuid (:uuid oldtask)]
-  (spit "../last-modified.data" uuid) 
+      uuid (:uuid oldtask)
+      data-path (str (fs/path (fs/parent *file*) (fs/path "last-modified.data")))]
+  (spit data-path uuid)
   (println (json/generate-string newtask)))
