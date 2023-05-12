fd7b165136f06fcd8c018401942c008ba0a261da
Somē Cho <itssomicho@gmail.com>
2023-05-12 11:31:09 +0200

    add projectsof script

---
+++ projectsof.clj
@@ -0,0 +1,25 @@
+#!/usr/bin/env bb
+(require '[clojure.java.shell :refer [sh]]
+         '[clojure.string :as str]
+         '[babashka.fs :as fs])
+
+(defn find-java-projects
+  []
+  (-> (sh "rg" 
+      "--type-add" "settings:settings.gradle"
+      "--type-add" "pom:pom.xml"
+      "-tsettings" "-tpom" "--files")
+      (:out)
+      (str/split #"\n")
+      (as-> paths (map #(str (fs/cwd) "/" %) paths))
+      (as-> fullpaths (map fs/parent fullpaths))
+      (as-> parent (mapv #(println (str %)) parent))))
+
+(defn assign [project-type]
+  (case project-type
+    "java" (find-java-projects)
+    (println "Project type not supported")))
+
+(assign (first *command-line-args*))
+
+
